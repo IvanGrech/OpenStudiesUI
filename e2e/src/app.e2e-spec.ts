daa40eb84e39101e3635945142e24126c58774a1
Ivan <ivan.grech1612@gmail.com>
2020-07-14 14:25:41 +0300

    Changed app name occurances to OpenStudies

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('UserApp app is running!');
+    expect(page.getTitleText()).toEqual('OpenStudies app is running!');
   });
 
   afterEach(async () => {
