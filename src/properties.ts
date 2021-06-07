187eb4e4fc4b0128185deb05184249108c476029
ivang <ivan.grech1612@gmail.com>
2021-06-07 20:29:11 +0300

    properties for external server deployment added

--- src/properties.ts
+++ src/properties.ts
@@ -1,3 +1,15 @@
+/*
 export const mainUrl: string = "http://localhost:80/OpenStudies/";
 export const loginUrl: string = "http://localhost:80/OpenStudies/login";
 export const coursesApiUrl: string = "http://localhost:80/OpenStudies/courses"
+*/
+
+export const mainUrl: string = "http://139.28.39.145:80/OpenStudies-0.01/";
+export const loginUrl: string = "http://139.28.39.145:80/OpenStudies-0.01/login";
+export const coursesApiUrl: string = "http://139.28.39.145:80/OpenStudies-0.01/courses"
+
+
+/*export const mainUrl: string = "http://localhost:80/OpenStudies-0.01/";
+export const loginUrl: string = "http://localhost:80/OpenStudies-0.01/login";
+export const coursesApiUrl: string = "http://localhost:80/OpenStudies-0.01/courses"*/
+
