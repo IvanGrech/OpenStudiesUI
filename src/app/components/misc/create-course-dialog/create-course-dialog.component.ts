0c492daefe0d5b2076e3572f6dcdd0d97b205092
ivang <ivan.grech1612@gmail.com>
2021-06-09 18:02:34 +0300

    validation on course name added, removed description

--- src/app/components/misc/create-course-dialog/create-course-dialog.component.ts
+++ src/app/components/misc/create-course-dialog/create-course-dialog.component.ts
@@ -3,6 +3,7 @@ import {MatDialogRef} from '@angular/material/dialog';
 import {CourseData} from 'src/app/models/courseData';
 import {CourseService} from 'src/app/services/course.service';
 import {Router} from '@angular/router';
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-create-course-dialog',
@@ -12,22 +13,33 @@ import {Router} from '@angular/router';
 export class CreateCourseDialogComponent {
 
   data = new CourseData("", "");
+  maxLength: number = 16;
+  minLength: number = 3;
+
+
+  createCourseFormControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(this.maxLength),
+    Validators.minLength(this.minLength)
+  ]);
 
   constructor(private courseService: CourseService,
               public dialogRef: MatDialogRef<CreateCourseDialogComponent>, private router: Router) {
   }
 
   onCreateClick(): void {
-    this.courseService.createCourse(this.data).subscribe(
-      response => {
-        if (this.router.url === '/mycourses') {
-          this.router.navigate(['/home']);
+    if (this.createCourseFormControl.errors == null) {
+      this.courseService.createCourse(this.data).subscribe(
+        response => {
+          if (this.router.url === '/mycourses') {
+            this.router.navigate(['/home']);
+          }
+        },
+        error => {
         }
-      },
-      error => {
-      }
-    )
-    this.dialogRef.close();
+      )
+      this.dialogRef.close();
+    }
   }
 }
 
